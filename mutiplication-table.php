35bd1ab44ac90119b29ae8c157dc8828ee4cca18
nabajitroy <nabajitroy@gmail.com>
2019-05-07 22:52:06 +0530

    Formation of table

--- mutiplication-table.php
+++ mutiplication-table.php
@@ -1,8 +1,9 @@
 <?php  
+ $input = $argv[1];
  $count = 0;  
  $num = 2;
  $prime_numbers =array();  
-   while ($count < 10 )  
+   while ($count < $input )  
         {  
            $div_count=0;  
               for ( $i=1; $i<=$num; $i++)  
@@ -20,19 +21,39 @@
                        $num=$num+1;  
           }  //End while 
 
-$table = array();
+//$table = array();
+
+foreach($prime_numbers as $key=>$value) {
+               if(strlen($value)==1) 
+                        print "     ". $value ;
+                    elseif(strlen($value)==2)
+                        print "  ".$value;
+                     elseif(strlen($value)==3)
+                       print " " .$value;
+                     else
+                       print " ".$value;
+
+
+}
+
+print "\n";
 foreach($prime_numbers as $key=>$left_value) {
       print $left_value;
       foreach($prime_numbers as $key=>$top_value){
-	    //  $table[$left_value][]= $left_value * $top_value;
-		    
-		
-		print "        ". $left_value * $top_value;
+	     // $table[$left_value][]= $left_value * $top_value;
+		$value =$left_value * $top_value;
+        	if(strlen($value)==1) 
+	        	print "    ". $value ;
+                    elseif(strlen($value)==2)
+                        print "   ".$value;
+                     elseif(strlen($value)==3)
+                       print "  " .$value;
+                     else
+                       print " ".$value;
 	   }
 print "\n";
 }
 
-//print_r($table);
 
 
  
